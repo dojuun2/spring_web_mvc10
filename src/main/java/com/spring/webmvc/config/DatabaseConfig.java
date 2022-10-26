ac9eb8a7d23c9db9d401c63cb706a8842b2a224b
dojuun2 <djp9816@naver.com>
2022-10-26 15:09:08 +0900

    [22.10.26] 프로젝트 초기 세팅, 데이터소스 설정

---
+++ src/main/java/com/spring/webmvc/config/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package com.spring.webmvc.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+// DB 접속 관련 설정 (접속, 커넥션풀...)
+@Configuration  // 설정파일로 인식하도록
+@PropertySource("classpath:db_info.properties")     // 프로퍼티즈 파일을 여기서 읽겠다
+public class DatabaseConfig {
+
+    @Value("${local.db.username}")
+    private String username;
+
+    @Value("${local.db.password}")
+    private String password;
+
+    @Value("${local.db.url}")
+    private String url;
+
+    @Bean
+    public DataSource dataSource(){   // <bean id="dataSource"
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setJdbcUrl(url);
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+
+        return new HikariDataSource(config);
+    }
+}
